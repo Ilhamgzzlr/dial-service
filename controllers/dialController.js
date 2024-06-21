91bd42d1a8227b85ec4318dd4e8e2479e8498311
SaktiWidyatmaja <saktiwidyatmaja1@gmail.com>
2024-06-21 16:04:53 +0700

    add todos to add ML model later

--- controllers/dialController.js
+++ controllers/dialController.js
@@ -1,6 +1,10 @@
 const { Dial } = require('../models');
 const { Op } = require('sequelize'); // Make sure to require Op from sequelize
 
+// TODO: add chosen library
+// const joblib = require('joblib');
+// const tf = require('@tensorflow/tfjs-node');
+
 class DialController {
   static async create(req, res, next) {
     try {
@@ -88,6 +92,7 @@ class DialController {
   static async findNextHandlingTimePrediction(req, res, next) {
     try {
       const agent_id = req.params.agent_id;
+      const dpd = req.body.DPD;
       const avgHandlingTime = await DialController.calculateAvgAgentHandlingTime(agent_id);
 
       if (avgHandlingTime == -1) {
@@ -96,10 +101,38 @@ class DialController {
         };
       }
 
+      // TODO: Pick the model type and uncomment based on the chosen type
+
+      // // Tf model
+      // const model = await tf.loadLayersModel('path_to_your_model/model.json');
+
+      // // Ubah fitur-fitur masukan ke dalam bentuk tensor TensorFlow
+      // const input = tf.tensor2d([[agent_id, dpd]]); // Ubah sesuai dengan format masukan yang diterima oleh model
+    
+      // // Lakukan prediksi menggunakan model
+      // const prediction = model.predict(input);
+    
+      // // Ambil hasil prediksi dan ubah menjadi nilai yang dapat dibaca
+      // const handlingTime = prediction.dataSync()[0]; // Ubah sesuai dengan format hasil prediksi yang diinginkan
+    
+      // // Hapus tensor yang tidak digunakan lagi
+      // tf.dispose(input);
+      // tf.dispose(prediction);    
+
+
+      // // Scikit model
+      // const model = joblib.load('path_to_your_model/model.pkl');
+      // const inputFeatures = [[agent_id, dpd]]; // Ubah sesuai dengan format masukan yang diterima oleh model
+      // const prediction = model.predict(inputFeatures); // Lakukan prediksi
+
+      // const handlingTime = prediction[0]; // Ubah sesuai dengan format hasil prediksi yang diinginkan
 
       res.status(200).json({
         STATUS: "SUCCESS",
         AGENT_ID: agent_id,
+        
+        // TODO: Change the output with proper handlingTime
+        // "NEXT HANDLING TIME PREDICTION": handlingTime,
         "NEXT HANDLING TIME PREDICTION": avgHandlingTime,
       });
     } catch (err) {
